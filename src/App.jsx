d000b0deba98e38a3ebd00773caa2e1601ada934
shuvo2o <eng.shuvo03@gmail.com>
2026-01-06 11:21:45 +0600

    password updated

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,20 @@ function App() {
 
   return (
     <>
-    <div className='p-5 container mx-auto'>
-      <nav className='py-28 bg-slate-50'>
-        <ul className='flex items-center justify-center space-x-4'>
-          <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/register"}>Register</Link></li>
-          <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/login"}>Login</Link></li>
-          <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/dashboard"}>Dashboard</Link></li>
-               <li><Link to="/user-profile" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Profile</Link></li>
-        </ul>
-      </nav>
-      <Logout/>
-      <AuthState/>
-    </div>
-     
+      <div className='p-5 container mx-auto'>
+        <nav className='py-28 bg-slate-50'>
+          <ul className='flex items-center justify-center space-x-4'>
+            <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/register"}>Register</Link></li>
+            <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/login"}>Login</Link></li>
+            <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/dashboard"}>Dashboard</Link></li>
+            <li><Link to="/user-profile" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Profile</Link></li>
+            <li><Link to="/reset-password" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Update Password</Link></li>
+          </ul>
+        </nav>
+        <Logout />
+        <AuthState />
+      </div>
+
     </>
   )
 }
