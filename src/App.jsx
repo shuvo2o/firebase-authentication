9cd773cf6131e2bbb67dcf7d5d4cd2dd841888c1
shuvo2o <eng.shuvo03@gmail.com>
2026-01-06 11:43:05 +0600

    send pass reset email

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
+import React from 'react'
 import { Link } from 'react-router'
-import './App.css'
 import Logout from './components/Logout'
-import AuthState from './components/AuthState'
 
-function App() {
+
+const App = () => {
 
   return (
     <>
       <div className='p-5 container mx-auto'>
-        <nav className='py-28 bg-slate-50'>
-          <ul className='flex items-center justify-center space-x-4'>
-            <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/register"}>Register</Link></li>
-            <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/login"}>Login</Link></li>
-            <li><Link className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700' to={"/dashboard"}>Dashboard</Link></li>
-            <li><Link to="/user-profile" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Profile</Link></li>
-            <li><Link to="/reset-password" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Update Password</Link></li>
-          </ul>
-        </nav>
-        <Logout />
-        <AuthState />
-      </div>
-
+          <nav className='py-28 mt-16 bg-slate-50'>
+            <ul className='flex items-center justify-center space-x-4'>
+              <li><Link to="/register" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Register</Link></li>
+              <li><Link to="/login" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Login</Link></li>
+              <li><Link to="/dashboard" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Dashboard</Link></li>
+              <li><Link to="/user-profile" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Profile</Link></li>
+              <li><Link to="/reset-password" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>Reset Password</Link></li>
+              <li><Link to="/password-less-signin" className='px-6 py-2 bg-blue-500 text-white rounded hover:bg-blue-700'>PasswordLess Sign-in</Link></li>
+            </ul>
+          </nav>
+          <Logout />
+        </div>
     </>
   )
 }
--- src/main.jsx
+++ src/main.jsx
@@ -11,19 +11,24 @@ import PrivateRoute from './routers/PrivateRoute.jsx'
 import UserProfile from './components/UserProfile.jsx'
 import UpdateProfile from './components/UpdateProfile.jsx'
 import UpdatePassword from './components/UpdatePassword.jsx'
-
+import SendPasswordResetEmail from './components/SendPasswordResetEmail.jsx'
+import SendSignInLink from './components/passwordless/SendSignInLink.jsx'
+import FinishSignIn from './components/passwordless/FinishSignIn.jsx'
 createRoot(document.getElementById('root')).render(
-  <AuthProvider>
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/login' element={<Login />} />
-        <Route path='/dashboard' element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-        <Route path='/user-profile' element={<PrivateRoute><UserProfile /></PrivateRoute>} />
-        <Route path='/update-user' element={<PrivateRoute><UpdateProfile /></PrivateRoute>} />
-        <Route path='/reset-password' element={<PrivateRoute><UpdatePassword /></PrivateRoute>} />
-      </Routes>
-    </BrowserRouter>
-  </AuthProvider>
+<AuthProvider>
+   <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path='/register' element={<Register/>}/>
+      <Route path='/login' element={<Login/>}/>
+      <Route path='/password-less-signin' element={<SendSignInLink/>}/>
+      <Route path='/finish-signup' element={<FinishSignIn/>}/>
+      <Route path='/dashboard' element={<PrivateRoute><Dashboard/></PrivateRoute> }/>
+      <Route path='/user-profile' element={<PrivateRoute><UserProfile/></PrivateRoute>}/>
+      <Route path='/update-user' element={<PrivateRoute><UpdateProfile/></PrivateRoute>}/>
+      <Route path='/reset-password' element={<PrivateRoute><UpdatePassword/></PrivateRoute>}/>
+      <Route path='/reset-password-email' element={<PrivateRoute><SendPasswordResetEmail/></PrivateRoute>}/>
+    </Routes>
+  </BrowserRouter>
+ </AuthProvider>,
 )
